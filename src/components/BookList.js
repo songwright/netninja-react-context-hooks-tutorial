e79b99c3442da11ed169c652d079f53e3de8ae88
Ramon Sanchez <songwright66@gmail.com>
2019-12-31 10:25:00 -0800

    Imported ThemeContext into BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className='book-list'>
+      <div className='book-list' style={{ color: theme.color, background: theme.bg}}>
         <ul>
-          <li>The Way of Kings</li>
-          <li>The Name of the Wind</li>
-          <li>The Final Emplire</li>
+          <li style={{ background: theme.ui }}>The Way of Kings</li>
+          <li style={{ background: theme.ui }}>The Name of the Wind</li>
+          <li style={{ background: theme.ui }}>The Final Emplire</li>
         </ul>
       </div>
     );
