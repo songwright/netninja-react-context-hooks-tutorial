46ea81f11cfc03d5b859467f0769bf0abfb1625a
Ramon Sanchez <songwright66@gmail.com>
2020-01-08 22:01:19 -0800

    Added toggleTheme function to ThemeContext.js

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -8,9 +8,12 @@ class ThemeContextProvider extends Component {
     light: { color: '#555', ui: '#ddd', bg: '#eee' },
     dark: { color: '#ddd', ui: '#333', bg: '#555' }
   }
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !isLightTheme });
+  }
   render() { 
     return (
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
         {this.props.children}
       </ThemeContext.Provider>
     );
