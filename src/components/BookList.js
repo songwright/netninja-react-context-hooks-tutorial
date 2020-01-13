756616b74f8801c4a829811fb2976e4274eb7b21
Ramon Sanchez <songwright66@gmail.com>
2020-01-13 15:07:56 -0800

    Imported BookContext into BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 const BookList = () => {
-  const {isLightTheme, light, dark} = useContext(ThemeContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div className='book-list' style={{ color: theme.color, background: theme.bg}}>
       <ul>
-        <li style={{ background: theme.ui }}>The Way of Kings</li>
-        <li style={{ background: theme.ui }}>The Name of the Wind</li>
-        <li style={{ background: theme.ui }}>The Final Emplire</li>
+        {books.map(book => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>{ book.title }</li>
+          )
+        })}
       </ul>
     </div>
   );
