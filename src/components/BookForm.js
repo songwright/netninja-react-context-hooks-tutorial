bbea531775548990e46279f8d30000822e0e2a36
Ramon Sanchez <songwright66@gmail.com>
2020-01-14 11:30:03 -0800

    Added handleSubmit function to BookForm.js

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -5,8 +5,14 @@ const NewBookForm = () => {
   const { addBook } = useContext(BookContext);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" placeholder="book title" value={title}
         onChange={e => setTitle(e.target.value)} required />
       <input type="text" placeholder="book author" value={author}
