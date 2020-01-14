a52c267db428ae476d4ea232cbc885110aff9138
Ramon Sanchez <songwright66@gmail.com>
2020-01-14 15:00:05 -0800

    Added localStorage with useEffect to BookContext.js

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,10 +1,13 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
   const [books, dispatch] = useReducer(bookReducer, []);
+  useEffect(() => {
+    localStorage.setItem('books', JSON.stringify(books))
+  }, [books]);
   return (
     // <BookContext.Provider value={{books, addBook, removeBook}}>
     <BookContext.Provider value={{books, dispatch}}>
