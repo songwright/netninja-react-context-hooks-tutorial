8060af64a22bc07b75a408e7362521e71b119426
Ramon Sanchez <songwright66@gmail.com>
2020-01-14 09:03:39 -0800

    Updated BookContext.js

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -2,18 +2,22 @@ import React, { createContext, useState } from 'react';
 
 export const BookContext = createContext();
 
-const BookContextProvider = (props) => {
+const BookContextProvider = props => {
   const [books, setBooks] = useState([
-    {title: 'name of the wind', id: 1},
-    {title: 'the way of kings', id: 2},
-    {title: 'the final empire', id: 3},
-    {title: 'the hero of ages', id: 4}
+    {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
+    {title: 'the final empire', author: 'brandon sanderson', id: 2}
   ]);
+  const addBook = (title, author) => {
+    setBooks([...books, {title, author, id: Math.random()}])
+  }
+  const removeBook = id => {
+    setBooks(books.filter(book => book.id !== id));
+  };
   return (
-    <BookContext.Provider value={{books}}>
-      {props.children}
+    <BookContext.Provider value={{books, addBook, removeBook}}>
+      { props.children }
     </BookContext.Provider>
-  );
+  )
 }
 
 export default BookContextProvider;
