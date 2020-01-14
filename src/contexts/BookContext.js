77b0d86c445a67909b1886500607f7eca0d66e32
Ramon Sanchez <songwright66@gmail.com>
2020-01-14 15:05:56 -0800

    Added localStorage parameter to useReducer in BookContext.js

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -4,7 +4,10 @@ import { bookReducer } from '../reducers/bookReducer';
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem('books');
+    return localData ? JSON.parse(localData) : [];
+  });
   useEffect(() => {
     localStorage.setItem('books', JSON.stringify(books))
   }, [books]);
